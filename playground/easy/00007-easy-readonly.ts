2b62091b0e2ca965768d612f194dd261ae5cc95a
takayuki murata <takayuki.for.public@gmail.com>
2026-08-17 15:36:29 +0900

    solve: 7 readonly

--- playground/easy/00007-easy-readonly.ts
+++ playground/easy/00007-easy-readonly.ts
@@ -29,7 +29,7 @@
 
 /* _____________ ここにコードを記入 _____________ */
 
-type MyReadonly<T> = any
+type MyReadonly<T> = { [P in keyof T]: readonly T[P] }
 
 /* _____________ テストケース _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
