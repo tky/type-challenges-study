e5a64aaa0671f41d7cd91262513a73a67c98b51d
takayuki murata <takayuki.for.public@gmail.com>
2026-08-17 10:32:19 +0900

    Finish 13!

--- playground/warm/00013-warm-hello-world.ts
+++ playground/warm/00013-warm-hello-world.ts
@@ -28,7 +28,7 @@
 
 /* _____________ ここにコードを記入 _____________ */
 
-type HelloWorld = any // expected to be a string
+type HelloWorld = string // expected to be a string
 
 /* _____________ テストケース _____________ */
 import type { Equal, Expect, NotAny } from '@type-challenges/utils'
