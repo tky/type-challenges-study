e1071aa690ee654ec432e17ad700f6df31f75b26
takayuki murata <takayuki.for.public@gmail.com>
2026-08-25 10:26:39 +0900

    Solve: medium 2

--- playground/medium/00002-medium-get-return-type.ts
+++ playground/medium/00002-medium-get-return-type.ts
@@ -25,7 +25,7 @@
 
 /* _____________ ここにコードを記入 _____________ */
 
-type MyReturnType<T> = any
+type MyReturnType<T> = T extends (...args: any[]) => infer R ? R : never; 
 
 /* _____________ テストケース _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
