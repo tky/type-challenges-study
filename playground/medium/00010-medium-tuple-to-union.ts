d0beca11c5dc25a1c1d1587ddbc9ba0a2ee076fc
takayuki murata <takayuki.for.public@gmail.com>
2026-09-04 16:42:14 +0900

    resolve medium 10

--- playground/medium/00010-medium-tuple-to-union.ts
+++ playground/medium/00010-medium-tuple-to-union.ts
@@ -20,7 +20,12 @@
 
 /* _____________ ここにコードを記入 _____________ */
 
-type TupleToUnion<T> = any
+/*
+type TupleToUnion<T extends any []> = T extends [infer F, ... infer Rest] 
+  ? F | TupleToUnion<Rest>
+  : never;
+*/
+type TupleToUnion<T extends any []> = T[number]
 
 /* _____________ テストケース _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
