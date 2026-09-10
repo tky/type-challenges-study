f3c420cb6d96a9b3c7362851331793aa268beb64
takayuki murata <takayuki.for.public@gmail.com>
2026-09-10 10:59:37 +0900

    resolve

--- playground/medium/00106-medium-trim-left.ts
+++ playground/medium/00106-medium-trim-left.ts
@@ -18,7 +18,10 @@
 
 /* _____________ ここにコードを記入 _____________ */
 
-type TrimLeft<S extends string> = any
+
+type Space = ' ' | '\n' | '\t'
+
+type TrimLeft<S extends string> = S extends `${Space}${infer R}` ? TrimLeft<R> : S
 
 /* _____________ テストケース _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
