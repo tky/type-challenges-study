bd2f2ce2852a6c3cae54e5c8954b8f0dca487877
takayuki murata <takayuki.for.public@gmail.com>
2026-09-21 09:37:04 +0900

    Resolve 110

--- playground/medium/00110-medium-capitalize.ts
+++ playground/medium/00110-medium-capitalize.ts
@@ -18,7 +18,20 @@
 
 /* _____________ ここにコードを記入 _____________ */
 
-type MyCapitalize<S extends string> = any
+
+type UpperMap = {
+  a: 'A', b: 'B', c: 'C', d: 'D', e: 'E', f: 'F',
+  g: 'G', h: 'H', i: 'I', j: 'J', k: 'K', l: 'L',
+  m: 'M', n: 'N', o: 'O', p: 'P', q: 'Q', r: 'R',
+  s: 'S', t: 'T', u: 'U', v: 'V', w: 'W', x: 'X',
+  y: 'Y', z: 'Z',
+}
+
+type ToUpper<C extends string> = C extends keyof UpperMap ? UpperMap[C] : C
+
+type MyCapitalize<S extends string> = S extends `${infer First}${infer Rest}`
+  ? `${ToUpper<First>}${Rest}` : S
+
 
 /* _____________ テストケース _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
