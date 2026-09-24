d7278d34b93f43aa4a9d94b99abc7c5cf9cf91e7
takayuki murata <takayuki.for.public@gmail.com>
2026-09-24 10:27:03 +0900

    resolve 119

--- playground/medium/00119-medium-replaceall.ts
+++ playground/medium/00119-medium-replaceall.ts
@@ -18,7 +18,11 @@
 
 /* _____________ ここにコードを記入 _____________ */
 
-type ReplaceAll<S extends string, From extends string, To extends string> = any
+type ReplaceAll<S extends string, From extends string, To extends string> =
+  From extends '' ? S
+  : S extends `${infer Prefix}${From}${infer Suffix}`
+  ? `${Prefix}${To}${ReplaceAll<Suffix, From, To>}`
+  : S
 
 /* _____________ テストケース _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
